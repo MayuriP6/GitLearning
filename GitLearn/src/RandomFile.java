21384c11e2eb6fbdbb4bf75e1e0b009e084923fe
Mayuri Pawar <pawar15mayuri@gmail.com>
2021-03-25 18:24:45 +0530

    local merging branch1

--- GitLearn/src/RandomFile.java
+++ GitLearn/src/RandomFile.java
@@ -6,7 +6,13 @@ public class RandomFile {
 	}
 
 	public static void Method2() {
-		System.out.println("first commit from branch1");
+		System.out.println("first commit from branch1 for repo merging");
+	}
+	public static void Method4() {
+		System.out.println("second commit from branch1 for local merging");
+	}
+	public static void Method5() {
+		System.out.println("third commit from branch1 for loacl merging");
 	}
 }
 
