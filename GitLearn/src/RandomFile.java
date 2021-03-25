a8933c5cabd5411eb1038f8df97de4c2cb49a6e3
Mayuri Pawar <pawar15mayuri@gmail.com>
2021-03-25 18:21:43 +0530

    Second commit from master

--- GitLearn/src/RandomFile.java
+++ GitLearn/src/RandomFile.java
@@ -8,6 +8,10 @@ public class RandomFile {
 	public static void Method1() {
 		System.out.println("first commit from master");
 	}
+	
+	public static void Method3() {
+		System.out.println("Second commit from master");
+	}
 
 	public static void Method2() {
 		System.out.println("first commit from branch1 for repo merging");
