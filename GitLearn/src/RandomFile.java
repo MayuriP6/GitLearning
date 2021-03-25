8fafe32da561178bf584bf0afcc8fbf6cff18d5a
Mayuri Pawar <pawar15mayuri@gmail.com>
2021-03-25 18:29:05 +0530

    pushing it to remove branch1 after local merging

--- GitLearn/src/RandomFile.java
+++ GitLearn/src/RandomFile.java
@@ -9,10 +9,15 @@ public class RandomFile {
 		System.out.println("first commit from branch1 for repo merging");
 	}
 	public static void Method4() {
-		System.out.println("second commit from branch1 for local merging");
+		System.out.println("fourth commit from branch1 for local merging");
 	}
 	public static void Method5() {
 		System.out.println("third commit from branch1 for loacl merging");
 	}
+	
+	public static void Method6() {
+		System.out.println("sixth commit from branch1 after  local merging and pushing it to branch 1 for remote merging");
+	}
+	
 }
 
