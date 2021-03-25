d93828d4f31134d0fe901383a8379402b8ae20ac
Mayuri Pawar <pawar15mayuri@gmail.com>
2021-03-25 18:18:17 +0530

    first commit from master

--- GitLearn/src/RandomFile.java
+++ GitLearn/src/RandomFile.java
@@ -4,5 +4,9 @@ public class RandomFile {
 	public static void main(String[] args) {
 		System.out.println("Print stmt");
 	}
+	
+	public static void Method1() {
+		System.out.println("first commit from master");
+	}
 
 }
