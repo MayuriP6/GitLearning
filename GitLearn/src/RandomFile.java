5d6cf2f5fed1d501193b62355b818e6d540f3940
Mayuri Pawar <pawar15mayuri@gmail.com>
2021-03-25 18:48:39 +0530

    for merging after rebasing

--- GitLearn/src/RandomFile.java
+++ GitLearn/src/RandomFile.java
@@ -19,5 +19,9 @@ public class RandomFile {
 		System.out.println("sixth commit from branch1 after  local merging and pushing it to branch 1 for remote merging");
 	}
 	
+	public static void Method8() {
+		System.out.println("Eigth commit from branch1 after  local rebasing and pushing it to branch 1 for remote merging");
+	}
+	
 }
 
